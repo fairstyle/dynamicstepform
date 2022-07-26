36f3ab2a9714915d1df7e764c9aa84d87d736233
Andrw <kuvickdabest@gmail.com>
2022-07-26 03:33:25 -0400

    Dynamicstepform class

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # stepform
+
+Whenever I start a new project, I don't know what to write for the first commit. After doing a “git init” there is technically nothing there...
+
+## Install
+
+```npm
+npm install dynamicstepform
+```
+
+# Usage
+
+```bash
+dynamicstepform
+```
+
+# Contributing
+If someone wants to add or improve something, I invite you to collaborate directly in this repository: [dynamicstepform](https://github.com/fairstyle/dynamicstepform)
+
+# License
+dynamicstepform is released under the [MIT License](https://opensource.org/licenses/MIT).
--- src/dynamicstepform.js
+++ src/dynamicstepform.js
@@ -1,17 +1,58 @@
 'use strict';
 
-console.log("Hola Mundo!");
-
 class Dynamicstepform {
-    constructor(...args) {
+
+    dynamicsteps = {
+        "currentStepIndex": null,
+        "steps": null,
+        "styleMotor": "tailwindcss"
+    }
+
+    tailwindSteps = {
+        "doneColorClass": "",
+        "currentColorClass": "",
+        "nextColorClass": "",
+    }
+
+    cssSteps = {
+        "doneColorClass": "",
+        "currentColorClass": "",
+        "nextColorClass": "",
+    }
+
+    constructor(steps = [], opts = {}) {
 
         // Prevent run in Node env
         if (typeof window === 'undefined') {
+            console.log("No nodejs 👌");
             return false;
         }
-        console.log("Stepform 👌");
+
+        this.opts(opts);
+        this.steps(steps);
+
+        steps.forEach((step) => {
+            console.log(step.step)
+            step.call();
+        })
+
+        console.log("created Stepform");
     }
+
+    create(element) {
+        console.log(element, "CREATE")
+        element.innerHTML("<h1>HOLAAAAA</h1>");
+    }
+
+    opts(opts = {}) {
+        console.log(opts);
+    }
+
+    steps(steps = []) {
+        console.log(steps);
+    }
+
+
 }
 
-Dynamicstepform.version = '1.0.0';
-module.export =  Dynamicstepform;
+module.exports =  Dynamicstepform;
